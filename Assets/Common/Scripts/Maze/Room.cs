f33f1077f40528f04570fa4b509b86deee914b1e
BRODI MATHERLY <69179454+amatherly@users.noreply.github.com>
2023-08-05 15:25:54 -0700

    Lots of reorganizing, tests, assemblies built

---
+++ Assets/Common/Scripts/Maze/Room.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using Singleton;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+
+    private Maze myMaze;
+
+    [SerializeField]
+    private bool myHasVisited;
+
+    [SerializeField]
+    private bool myWinRoom;
+
+    [SerializeField]
+    private int myRow;
+
+    [SerializeField]
+    private int myCol;
+
+    [SerializeField]
+    private List<GameObject> myDoors;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMaze = GameObject.FindGameObjectWithTag("Maze").GetComponent<Maze>();
+        myHasVisited = false;
+    }
+    
+
+    public string GetQuestion()
+    {
+        return "default text, no question loaded";
+    }
+
+    public int MyRow
+    {
+        get => myRow;
+    }
+
+    public int MyCol
+    {
+        get => myCol;
+    }
+
+    public List<GameObject> MyDoors
+    {
+        get => myDoors;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myMaze.MyCurrentRoom = this;
+            myHasVisited = true;
+            if (myWinRoom)
+            {
+                //win message or something idk
+            }
+        }
+    }
+}
---
+++ Assets/Common/Scripts/Question/QuestionWindowController.cs
@@ -0,0 +1,118 @@
+
+using System.Linq;
+using Singleton;
+using UnityEngine;
+using Random = System.Random;
+
+
+public class QuestionWindowController : MonoBehaviour
+{
+    private static Random RANDOM = new Random();
+    private static UIControllerInGame myUIController;
+    private static Maze MAZE = Maze.MyInstance;
+
+    private static int POPUP_SOUND = 0;
+    private static int CORRECT_SOUND = 2;
+    private static int INCORRECT_SOUND = 3;
+    
+    private QuestionWindowView myView;
+    private Question myQuestion;
+    
+    private string myAnswerInput;
+    private int myCorrectIndex;
+    private bool myIsCorrect;
+    
+    [SerializeField] private GameObject TFWindowPrefab;
+    [SerializeField] private GameObject multipleChoiceWindowPrefab;
+    [SerializeField] private GameObject inputFieldWindowPrefab;
+
+
+    public void InitializeWindow(Question theQuestion)
+    {
+        UIControllerInGame.MyInstance.PlayUISound(0);
+        
+        myQuestion = theQuestion;
+        Debug.Log("Initializing window with question: " + myQuestion);
+
+        myIsCorrect = false;
+        int ID = theQuestion.MyQuestionID;
+        
+        switch (ID)
+        {    case 1:
+                InstantiateTFWindow();
+                break;
+            case 2:
+                InstantiateMultipleChoiceWindow();
+                break;
+            case 3:
+                InstantiateInputFieldWindow();
+                break;
+        }
+    }
+    
+    private void InstantiateTFWindow()
+    {
+        GameObject multipleChoiceWindow = Instantiate(TFWindowPrefab, transform);
+        myView = multipleChoiceWindow.GetComponent<QuestionWindowView>();
+        myView.InitializeView();
+        myView.SetQuestionText(myQuestion.MyQuestion);
+    }
+
+    private void InstantiateMultipleChoiceWindow()
+    {
+        GameObject multipleChoiceWindow = Instantiate(multipleChoiceWindowPrefab, transform);
+        myView = multipleChoiceWindow.GetComponent<QuestionWindowView>();
+        myView.InitializeView();
+
+        string[] words = myQuestion.MyAnswer.Split(',');
+        string[] randomizedAnswers = words.OrderBy(x => RANDOM.Next()).ToArray();
+        myQuestion.MyAnswer = words[0];
+        myView.SetQuestionText(myQuestion.MyQuestion);
+        myView.SetMultipleChoiceButtons(randomizedAnswers);
+    }
+    
+    private void InstantiateInputFieldWindow()
+    {
+        GameObject inputFieldWindow = Instantiate(inputFieldWindowPrefab, transform);
+        myView = inputFieldWindow.GetComponent<QuestionWindowView>();
+        myView.InitializeView();
+        myView.SetQuestionText(myQuestion.MyQuestion);
+        myView.EnableInputField();
+    }
+
+    public void CheckAnswer()
+    {
+        Debug.Log("Checking the answer for question: " + myQuestion);
+        myIsCorrect = myQuestion.CheckUserAnswer(myAnswerInput);
+        myView.ShowResult(myIsCorrect);
+
+        if (myIsCorrect)
+        {
+            UIControllerInGame.MyInstance.PlayUISound(CORRECT_SOUND);
+        }
+        else
+        {
+            UIControllerInGame.MyInstance.PlayUISound(INCORRECT_SOUND);
+        }
+        
+        
+        Maze.MyInstance.SetDoorLockState(myIsCorrect);
+        QuestionFactory.MyInstance.RemoveCurrentQuestion();
+        Destroy(myView.gameObject);
+    }
+
+    public void SetAnswerInput(string theAnswerInput)
+    {
+        Debug.Log("Setting Answer for question: " + myQuestion);
+        
+        if (myQuestion.MyQuestionID == 2)
+        {
+            theAnswerInput = myView.GetButtonAnswer(theAnswerInput);
+        }
+        if (theAnswerInput != null)
+        {
+            myAnswerInput = theAnswerInput;
+            CheckAnswer();
+        }
+    }
+}
