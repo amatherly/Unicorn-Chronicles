1753d8a66039a665d1ea4e799adf19838a68ab6a
Caroline E <82617020+carolinenajwa@users.noreply.github.com>
2023-08-09 12:20:28 -0700

    remove playerpref

--- Assets/Common/Scripts/Maze/DoorController.cs
+++ Assets/Common/Scripts/Maze/DoorController.cs
@@ -1,3 +1,4 @@
+using System;
 using Common.Scripts.Controller;
 using Singleton;
 using UnityEngine;
@@ -21,6 +22,8 @@ namespace Common.Scripts.Maze
             myDoor = GetComponent<Door>();
             myQuestionFactory = QuestionFactory.MyInstance;
             myMaze = GameObject.Find("Maze").GetComponent<global::Maze>();
+            myDoorID = Guid.NewGuid().ToString();
+
         }
 
         void Update()
