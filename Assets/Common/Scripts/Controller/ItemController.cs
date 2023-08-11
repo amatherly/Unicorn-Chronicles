b60ed1375878f9af39d75635e55122de46475051
JJ Coldiron <jj.coldiron@outlook.com>
2023-08-11 12:24:22 -0700

    Added doc

--- Assets/Common/Scripts/Controller/ItemController.cs
+++ Assets/Common/Scripts/Controller/ItemController.cs
@@ -2,27 +2,50 @@ using Common.Scripts.Controller;
 using Singleton;
 using UnityEngine;
 
+/// <summary>
+/// Simple class to increment the <c>myItemCount</c> field of the
+/// <c>Player</c> script and handle animation.
+/// </summary>
 public class ItemController : MonoBehaviour
 {
+
+    /// <summary>
+    /// Speed of the item's bobbing animation.
+    /// </summary>
     public static readonly float SPEED = 1.5f;
 
+    /// <summary>
+    /// Height variance of the item's bobbing animation.
+    /// </summary>
     public static readonly float HEIGHT = 0.0025f;
 
+    /// <summary>
+    /// Reference to the <c>Player</c> script.
+    /// </summary>
     private PlayerController myPlayer;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Start is called before the first frame update.
+    /// </summary>
     void Start()
     {
         myPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Update is called once per frame.
+    /// </summary>
     void Update()
     {
         float newY = Mathf.Sin(Time.time * SPEED) * HEIGHT + transform.position.y;
         transform.position = new Vector3(transform.position.x, newY, transform.position.z);
     }
 
+    /// <summary>
+    /// Called when the player enters the item's collider to increment
+    /// the player's <c>myItemCount</c> and destroy the <c>GameObject</c>.
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
--- Assets/Common/Scripts/Maze/MinimapCell.cs
+++ Assets/Common/Scripts/Maze/MinimapCell.cs
@@ -1,26 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
-using Singleton;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Script to handle proper color change of the rooms
+/// on the minimap.
+/// </summary>
 public class MinimapCell : MonoBehaviour
 {
 
+    /// <summary>
+    /// The <c>Room</c> the minimap cell corresponds to.
+    /// </summary>
     [SerializeField]
     private GameObject myRoom;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
     void Start()
     {
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
     void Update()
     {
         ColorChange();
     }
 
+    /// <summary>
+    /// Changes the color of the cell depending on the state of its
+    /// corresponding <c>Room</c>. White for unvisited rooms, green if
+    /// a room has been visited, and blue if the player is currently in
+    /// the room.
+    /// </summary>
     internal void ColorChange()
     {
         Color newColor = Color.white;
