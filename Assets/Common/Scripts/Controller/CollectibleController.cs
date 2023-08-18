7bb293d92c4d107af4b9585177b4bc57facfa8b9
Caroline E <82617020+carolinenajwa@users.noreply.github.com>
2023-08-18 10:15:30 -0700

    removed namespace

--- Assets/Common/Scripts/Controller/CollectibleController.cs
+++ Assets/Common/Scripts/Controller/CollectibleController.cs
@@ -6,35 +6,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Common.Scripts.Controller
+
+/// <summary>
+/// Manages collectible items in the game.
+/// </summary>
+/// <author>JJ Coldiron</author>
+/// <author>Caroline El Jazmi</author>
+/// <author>Brodi Matherly</author>
+/// <remarks>
+/// Developed using Unity [Version 2021.3.23f1].
+/// </remarks>
+public class CollectibleController : MonoBehaviour
 {
     /// <summary>
-    /// Manages collectible items in the game.
+    /// List of all collectible items in the game.
     /// </summary>
-    /// <author>JJ Coldiron</author>
-    /// <author>Caroline El Jazmi</author>
-    /// <author>Brodi Matherly</author>
-    /// <remarks>
-    /// Developed using Unity [Version 2021.3.23f1].
-    /// </remarks>
-    public class CollectibleController : MonoBehaviour
+    public List<ItemController>
+        allMyItems = new();
+
+
+    /// <summary>
+    /// Automatically populates the list of collectible items and performs initialization.
+    /// </summary>
+    private void Start()
     {
-        /// <summary>
-        /// List of all collectible items in the game.
-        /// </summary>
-        public List<ItemController>
-            allMyItems = new(); 
-        
-        
-        /// <summary>
-        /// Automatically populates the list of collectible items and performs initialization.
-        /// </summary>
-        private void Start()
-        {
-            // Auto-populate the allMyKeys list with every Key in the scene
-            allMyItems.AddRange(FindObjectsOfType<ItemController>());
-        }
-        
+        // Auto-populate the allMyKeys list with every Key in the scene
+        allMyItems.AddRange(FindObjectsOfType<ItemController>());
     }
-    
+
 }
